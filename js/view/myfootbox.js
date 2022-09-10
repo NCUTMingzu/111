64506ba37b2bae8fe4837dce4428e55fa168aaf8
did1335 <did1335@gmail.com>
2022-09-10 17:45:23 +0800

    111

---
+++ js/view/myfootbox.js
@@ -0,0 +1,15 @@
+Vue.component('footbox', {
+    template: `<div id="myfoot_box">
+        <article>
+        國立勤益科技大學National Chin-Yi University of Technology<br>
+        地址：<a href="https://www.ncut.edu.tw/p/412-1000-253.php?Lang=zh-tw" title="交通資訊" target="_blank">41170臺中市太平區坪林里中山路二段 57號 (交通資訊)</a> | 
+        <a href="https://www.ncut.edu.tw/p/412-1000-239.php?Lang=zh-tw" title="勤益校園地圖" target="_blank">校園地圖</a><br>
+        No.57, Sec. 2, Zhongshan Rd., Taiping Dist., Taichung 41170, Taiwan (R.O.C.)<br>
+        電話: <a href="https://www.ncut.edu.tw/ncut/phone/" target="_blank">(04)23924505 (分機表)</a>FAX:(04) 23923363 TANet 98110000 <br>
+        <a href="https://web2.ncut.edu.tw/ezfiles/0/1000/img/949/878419923.pdf" target="_blank">個人資料保護安全政策 </a> | 
+        <a href="https://web2.ncut.edu.tw/files/11-1000-2878.php" target="_blank">隱私權聲明</a> | 
+        <!--<a href="https://web2.ncut.edu.tw/files/11-1000-5908.php" target="_blank">當事人行使權利方式及 抱怨申訴</a> |--> 
+        <a href="https://web2.ncut.edu.tw/ezfiles/0/1000/img/949/204756527.pdf" target="_blank">個資法17條應公告事項</a>
+        </article>
+    </div>`,
+});
